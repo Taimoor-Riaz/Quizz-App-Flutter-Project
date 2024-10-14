b703a3d059ddf48e82fa33755d8e16cddd13f582
Taimoor-Riaz <mughaltaimoor298@gmail.com>
2024-10-14 23:45:09 +0500

    questions

---
+++ lib/Data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:quiz_app/Model/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions(
+    "What are the main building blocks of flutter",
+    [
+      "widgets",
+      "Components",
+      "Matterial",
+      "None of Above",
+    ],
+  )
+];
